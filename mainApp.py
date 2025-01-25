495606bf9e1120b96b4bebbec7ef48faf932501c
harrry1412 <hchen1412@gmail.com>
2025-01-25 15:48:07 -0500

    Update mainApp.py

--- mainApp.py
+++ mainApp.py
@@ -16,7 +16,7 @@ import re
 CONFIG_FILENAME = "config.json"
 ICON_FILENAME = "app_icon.png"
 APP_NAME = "VIVA自提单自动生成工具 V2.1.0"
-APP_TITLE = f"{APP_NAME} - Designed by Harry"
+APP_TITLE = f"{APP_NAME} - Designed by Harry & Zeror"
 
 
 class DataExtractorApp(QWidget):
